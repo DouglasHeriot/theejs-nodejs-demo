c3eb5cc712f2c5fc7ada51455c599a4025d82a88
Douglas Heriot <douglas@douglasheriot.com>
2013-03-20 09:52:57 +1100

    Fixed loading socket.io script from correct domain/port

--- server.js
+++ server.js
@@ -9,25 +9,11 @@ app.configure(function(){
 		app.use(express.static(__dirname + '/static'));
 		});
 
-/*app.get('/static/*', function(req, res){ 
-  res.sendfile('static/index.html'); 
-});*/ 
-
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-
-  socket.on('resize', function(data){
-  	console.log(data);
-	socket.broadcast.emit('resize', data);
-  });
-
   socket.on('move', function(data){
 	  console.log("Moving box to x = ", data.x);
 	  socket.broadcast.emit('move', data);
